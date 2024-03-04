3a31a2bc5e9fbf9a90ed2aed20f13b3bf6485d4b
Elvis Wong <40566101+ElvisWong213@users.noreply.github.com>
2024-03-04 01:35:41 +0000

    finish first page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,65 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
+// import reactLogo from "./assets/react.svg";
+// import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { once } from "@tauri-apps/api/event";
+import { open } from "@tauri-apps/api/dialog";
+import { convertFileSrc } from "@tauri-apps/api/tauri";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  // const [greetMsg, setGreetMsg] = useState("");
+  // const [name, setName] = useState("");
+  const [selectedImage, setSelectedImage] = useState("");
+  // async function greet() {
+  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  //   setGreetMsg(await invoke("greet", { name }));
+  // }
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  const removeSelectedImage = () => {
+    setSelectedImage("");
+  };
+
+  const selectImage = () => {
+    let file = open({
+      multiple: false,
+      filters: [{
+        name: "Image",
+        extensions: ['png', 'jpeg']
+      }]
+    });
+    file.then((url) => {
+      if (url != null && !Array.isArray(url)) {
+        setImage(url);
+      }
+    })
+  };
+
+  function setImage(url: string) {
+    let src = convertFileSrc(url);
+    setSelectedImage(src);
   }
+
+  once('tauri://file-drop', event => {
+    let urls: string[] = event.payload as string[];
+    setImage(urls[0])
+  });
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="dropzone">
+        {
+          selectedImage == "" ?
+          <div className="dropzone_text">
+            <p>Drop image here or</p>
+            <button type="button" onClick={selectImage}>select image</button>
+          </div> :
+          <img src={selectedImage} onClick={selectImage}/>
+        }
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-      <p>Hello world</p>
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      <div className="buttons">
+        <button type="button" onClick={removeSelectedImage}>Clear</button>
+        <button type="button">Convert</button>
+      </div>
     </div>
   );
 }
