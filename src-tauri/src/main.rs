3f8d3778bd06d1afaa73bd256f4a6c7ca106ab3b
Elvis Wong <40566101+ElvisWong213@users.noreply.github.com>
2024-03-08 01:43:12 +0000

    feat: implement icon resize library

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use icon_image::ImageProcess;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -7,9 +8,19 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command(async)]
+fn convert_image(input_path: String, output_path: String) -> Result<String, String> {
+   let mut process = ImageProcess::new(input_path, output_path);
+   match process.run() {
+       Ok(path) => Ok(path),
+       Err(error) => Err(error.to_string())
+   }
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![convert_image])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,117 @@
 import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import { invoke } from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import { once } from "@tauri-apps/api/event";
-import { open } from "@tauri-apps/api/dialog";
+import { open, save } from "@tauri-apps/api/dialog";
 import { convertFileSrc } from "@tauri-apps/api/tauri";
+import { appCacheDir, downloadDir } from "@tauri-apps/api/path";
+import { readBinaryFile, writeBinaryFile } from "@tauri-apps/api/fs";
 
 function App() {
-  // const [greetMsg, setGreetMsg] = useState("");
-  // const [name, setName] = useState("");
   const [selectedImage, setSelectedImage] = useState("");
-  // async function greet() {
-  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  //   setGreetMsg(await invoke("greet", { name }));
-  // }
+  const [selectedImagePath, setSelectedImagePath] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [exportFilePath, setExportFilePath] = useState("");
+
+  async function convertImage() {
+    if (selectedImagePath == "") {
+      setErrorMessage("No image selected");
+      return;
+    }
+    setErrorMessage("");
+    let output = await appCacheDir().then((dir) =>
+      dir.substring(0, dir.length - 1),
+    );
+    console.log(output);
+    await invoke("convert_image", {
+      inputPath: selectedImagePath,
+      outputPath: output,
+    })
+      .then((url) => {
+        if (typeof url === "string") {
+          setExportFilePath(url);
+        }
+      })
+      .catch((error) => setErrorMessage(error));
+    // console.log(exportFilePath);
+    let zipFile = await readBinaryFile(exportFilePath, undefined);
+    let downloadPath = await downloadDir();
+    let savePath = await save({
+      defaultPath: downloadPath + "/" + "output.zip",
+      filters: [
+        {
+          name: "zip",
+          extensions: ["zip"],
+        },
+      ],
+    });
+    if (savePath == null) {
+      return;
+    }
+    // console.log(zipFile.length);
+    await writeBinaryFile(savePath, zipFile, undefined);
+  }
 
   const removeSelectedImage = () => {
     setSelectedImage("");
+    setSelectedImagePath("");
   };
 
   const selectImage = () => {
     let file = open({
       multiple: false,
-      filters: [{
-        name: "Image",
-        extensions: ['png', 'jpeg']
-      }]
+      filters: [
+        {
+          name: "Image",
+          extensions: ["png", "jpeg"],
+        },
+      ],
     });
     file.then((url) => {
       if (url != null && !Array.isArray(url)) {
         setImage(url);
       }
-    })
+    });
   };
 
   function setImage(url: string) {
     let src = convertFileSrc(url);
     setSelectedImage(src);
+    setSelectedImagePath(url);
   }
 
-  once('tauri://file-drop', event => {
+  once("tauri://file-drop", (event) => {
     let urls: string[] = event.payload as string[];
-    setImage(urls[0])
+    setImage(urls[0]);
   });
 
   return (
     <div className="container">
+      <p>{errorMessage}</p>
       <div className="dropzone">
-        {
-          selectedImage == "" ?
+        {selectedImage == "" ? (
           <div className="dropzone_text">
             <p>Drop image here or</p>
-            <button type="button" onClick={selectImage}>select image</button>
-          </div> :
-          <img src={selectedImage} onClick={selectImage}/>
-        }
+            <button type="button" onClick={selectImage}>
+              select image
+            </button>
+          </div>
+        ) : (
+          <img src={selectedImage} onClick={selectImage} />
+        )}
       </div>
       <div className="buttons">
-        <button type="button" onClick={removeSelectedImage}>Clear</button>
-        <button type="button">Convert</button>
+        <button type="button" onClick={removeSelectedImage}>
+          Clear
+        </button>
+        <button
+          type="button"
+          onClick={async () => {
+            await convertImage();
+          }}
+        >
+          Convert
+        </button>
       </div>
     </div>
   );
